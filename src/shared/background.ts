a7284817bf54b12a6e0f8b7229b3e9a0d1c68313
Jake Demian <jakedemian@gmail.com>
2024-11-16 13:31:28 -0700

    it works (with a few bugs)

--- src/shared/background.ts
+++ src/shared/background.ts
@@ -1,3 +1,62 @@
-chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-  console.log("Navigating to URL:", details.url);
-});
+chrome.webNavigation.onBeforeNavigate.addListener(
+  (details) => {
+    if (details.frameId !== 0) {
+      return;
+    }
+
+    const originalUrl = details.url;
+    const url = new URL(originalUrl);
+
+    const redditDomains = [
+      "reddit.com",
+      "reddit.co.uk",
+      "reddit.fr",
+      "reddit.de",
+      "reddit.jp",
+      "reddit.in",
+      "reddit.ca",
+      "reddit.ru",
+    ];
+
+    if (!redditDomains.some((domain) => url.hostname.endsWith(domain))) {
+      return;
+    }
+
+    const hostnameParts = url.hostname.split(".");
+    const domainParts = hostnameParts.slice(-2);
+
+    if (domainParts[1].length <= 2) {
+      domainParts.unshift(hostnameParts[hostnameParts.length - 3]);
+    }
+
+    const subdomains = hostnameParts.slice(
+      0,
+      hostnameParts.length - domainParts.length
+    );
+
+    const specialSubdomains = ["www", "new", "old"];
+
+    let shouldRedirect = false;
+
+    if (subdomains.length > 0) {
+      const subdomain = subdomains.join(".");
+
+      if (!specialSubdomains.includes(subdomain)) {
+        if (url.pathname.startsWith("/r/")) {
+          url.hostname = domainParts.join(".");
+        } else {
+          url.hostname = domainParts.join(".");
+          url.pathname = `/r/${subdomain}${url.pathname}`;
+        }
+        shouldRedirect = true;
+      }
+    }
+
+    if (shouldRedirect) {
+      if (originalUrl !== url.toString()) {
+        chrome.tabs.update(details.tabId, { url: url.toString() });
+      }
+    }
+  },
+  { url: [{ urlMatches: ".*" }] }
+);
